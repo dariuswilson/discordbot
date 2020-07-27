34ade1802654431b24339d8225290286bea6510a
Cheetah <dariuswilsonxu@gmail.com>
2020-07-27 16:47:23 -0500

    da

--- commands/ping.js
+++ commands/ping.js
@@ -1,14 +1,13 @@
 const Discord = require('discord.js');
-const botconfig = require('../botsettings.json');
 
 module.exports.run = async (bot, message, args) => {
 	return message.channel.send("Pong!");
-}
+};
 
 module.exports.config = {
 	name: "ping",
 	description: "",
 	usage: "!ping",
 	accessableby: "Members",
-	aliases: [],
+	aliases: []
 };
--- index.js
+++ index.js
@@ -1,49 +1,43 @@
 const Discord = require('discord.js');
-const botsettings = require('./botsettings.json');
-
 const bot = new Discord.Client({disableEveryone: true});
-
-bot.on("guildMemberAdd", member => {
-    const welcomeChannel = member.guild.channels.cache.find(channel => channel.name === 'welcome')
-    welcomeChannel.send (`Welcome! ${member}`)
-})
-
-require("./util/eventHandler")(bot)
-
-const fs = require("fs");
+const botsettings = require('./botsettings.json');
+const fs = require('fs');
+const { brotliCompress } = require('zlib');
+require('./util/eventHandler')(bot);
 bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
 
-fs.readdir("./commands/", (err, files) => {
+fs.readdir('./commands/', (err, files) => {
 
-    if(err) console.log(err)
+	if(err) console.log(err);
 
-    let jsfile = files.filter(f => f.split(".").pop() === "js") 
-    if(jsfile.length <= 0) {
-         return console.log("[LOGS] Couldn't Find Commands!");
-    }
+	const jsfile = files.filter(f => f.split('.').pop() === 'js');
+	if(jsfile.length <= 0) {
+		return console.log('[LOGS] Couldn\'t Find Commands!');
+	}
 
-    jsfile.forEach((f, i) => {
-        let pull = require(`./commands/${f}`);
-        bot.commands.set(pull.config.name, pull);  
-        pull.config.aliases.forEach(alias => {
-            bot.aliases.set(alias, pull.config.name)
-        });
-    });
+	jsfile.forEach((f, i) => {
+		const pull = require(`./commands/${f}`);
+		bot.commands.set(pull.config.name, pull);
+		pull.config.aliases.forEach(alias => {
+			bot.aliases.set(alias, pull.config.name);
+		});
+	});
 });
 
-bot.on("message", async message => {
-    if(message.author.bot || message.channel.type === "dm") return;
+bot.on('message', async message => {
+	if(message.author.bot || message.channel.type === 'dm') return;
 
-    let prefix = botsettings.prefix;
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0];
-    let args = message.content.substring(message.content.indexOf(' ')+1);
+	const prefix = botsettings.prefix;
+	const messageArray = message.content.split(' ');
+	const cmd = messageArray[0];
+	const args = message.content.substring(message.content.indexOf(' ') + 1);
 
-    if(!message.content.startsWith(prefix)) return;
-    let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
-    if(commandfile) commandfile.run(bot,message,args)
+	if(!message.content.startsWith(prefix)) return;
+	const commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)));
+	if(commandfile) commandfile.run(bot, message, args);
 
-})
+});
+
 
 bot.login(process.env.token);
