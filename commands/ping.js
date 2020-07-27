ae7695bd86b71834852f58fbfe466fca8f936249
Cheetah <dariuswilsonxu@gmail.com>
2020-07-27 16:34:49 -0500

    xd

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const botconfig = require('../config.json');
+const botconfig = require('../botsettings.json');
 
 module.exports.run = async (bot, message, args) => {
 	return message.channel.send("Pong!");
