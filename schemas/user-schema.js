b5fbdc8a6483351cdbdd4c766aa0bd773545a783
Cheetah <dariuswilsonxu@gmail.com>
2020-07-28 11:39:39 -0500

    d

--- schemas/user-schema.js
+++ schemas/user-schema.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 
+const reqString = {
+    typeString,
+    required: true
+}
+
 const userSchema = mongoose.Schema({
-    email: String,
-    username: String,
-    password: String
+    email: reqString,
+    username: reqString,
+    password: reqstring
 })
 
 module.exports = mongoose.model('users', userSchema)
