71afd9e157983ea6d17fb8152c30e47442595e50
Cheetah <dariuswilsonxu@gmail.com>
2020-07-28 11:15:43 -0500

    d

--- index.js
+++ index.js
@@ -10,11 +10,20 @@ client.commands = new Discord.Collection();
 
 // MONGODB
 const mongo = require('./mongo');
+const userSchema = require('./schemas/user-schema')
 
 const connectToMongoDB = async () => {
-	await mongo().then((mongoose) => {
+	await mongo().then(async (mongoose) => {
 	try {
-		console.log('Connected to MongoDB!');
+		console.log('Connected to MongoDB!')
+
+		const user = {
+			email: 'testingteemoo123@gmail.com',
+			username: 'Darius',
+			password: 'PAssword1!'
+		}
+
+		await new userSchema(user).save()
 	} finally {
 		mongoose.connection.close()
 	}
--- mongo.js
+++ mongo.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
-// const { mongoPath } = require('./config.json')
-
-const mongoPath = 'mongodb+srv://Darius:oldspice123@discordbot.3bz6j.mongodb.net/discordbot?retryWrites=true&w=majority'
+const { mongoPass } = require('./config.json')
+const mongoPath = `mongodb+srv://Darius:${mongoPass}@discordbot.3bz6j.mongodb.net/discordbot?retryWrites=true&w=majority`
 
 module.exports = async () => {
     await mongoose.connect(mongoPath, {
