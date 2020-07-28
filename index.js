515f42a80345b4595ec7524c6a80f4f22272993c
Cheetah <dariuswilsonxu@gmail.com>
2020-07-28 02:39:07 -0500

    a

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const client = new Discord.Client();
 const botsettings = require('./botsettings.json');
 const fs = require('fs');
 const { prefix } = require('./botsettings.json')
@@ -7,21 +7,21 @@ const { brotliCompress } = require('zlib');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://Darius:oldspice123@discordbot.3bz6j.mongodb.net/Data', { useNewUrlParser: true, useUnifiedTopology: true });
 
-bot.commands = new Discord.Collection();
+client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for(const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 
-	bot.commands.set(command.name, command);
+	client.commands.set(command.name, command);
 }
 
 
-bot.once('ready', () => { 
+client.once('ready', () => { 
 	console.log('I am online!');
 });
 
-bot.on('message', message => {
+client.on('message', message => {
 	if(!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).split(/ +/);
