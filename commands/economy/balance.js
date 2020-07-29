37ba7e8378e9419f6d96066e771e20aaabb942cc
Cheetah <dariuswilsonxu@gmail.com>
2020-07-28 20:17:49 -0500

    a

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -5,7 +5,7 @@ module.exports = {
   commands: ['balance', 'bal'],
   maxArgs: 1,
   expectedArgs: "[Target user's @]",
-  callback: async (message) => {
+  callback: async (message, args, text) => {
     const target = message.mentions.users.first() || message.author
     const targetId = target.id
 
