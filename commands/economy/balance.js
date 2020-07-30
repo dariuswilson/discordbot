c23c9cf312612e9c51422eca59a2adea1341070d
Cheetah <dariuswilsonxu@gmail.com>
2020-07-30 12:11:40 -0500

    d

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -6,24 +6,25 @@ module.exports = {
   maxArgs: 1,
   expectedArgs: "[Target user's @]",
   callback: async (message, args, text) => {
-    const target = message.mentions.users.first() || message.author
-   
-    const userId = target.id
-
-    const coins = await economy.getCoins(userId)
-
-    if(message.author) {
+    if(!message.mentions.user.first()) {
+      const userId = message.author.id;
+      const coins = await economy.getCoins(userId);
       const embed = new Discord.MessageEmbed()
       .setAuthor(message.member.user.tag, message.author.avatarURL())
       .setColor(0x00FF00)
       .setDescription(`You have ${coins} coins!`);
       message.channel.send(embed)
-    } else if (message.mentions.users.first()) {
+      return;
+    } else {
+      const target = message.mentions.users.first();
+      const userId = target.id;
+      const coins = await economy.getCoins(userId);
       const embed = new Discord.MessageEmbed()
       .setAuthor(message.member.user.tag, message.author.avatarURL())
       .setColor(0x00FF00)
       .setDescription(`That user has ${coins} coins!`);
       message.channel.send(embed)
+      return;
     }
   }
 }
