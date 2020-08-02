9fb2777a502917c2fd5c5b95e2e26ac79878844c
Cheetah <dariuswilsonxu@gmail.com>
2020-08-02 14:51:06 -0500

    s

--- commands/economy/pay.js
+++ commands/economy/pay.js
@@ -1,15 +1,16 @@
 const Discord = require('discord.js');
 const economy = require('../../economy')
 
-const currentAmount = economy.getBalance(message.author.id);
-const transferAmount = commandArgs.split(/ +/g).find(arg => !/<@!?\d+>/g.test(arg));
-const transferTarget = message.mentions.users.first();
 
 module.exports = {
     commands: ['pay'],
     maxArgs: 1,
     expectedArgs: "[Target user's @]",
     callback: async (message, args, text) => {
+const currentAmount = economy.getBalance(message.author.id);
+const transferAmount = commandArgs.split(/ +/g).find(arg => !/<@!?\d+>/g.test(arg));
+const transferTarget = message.mentions.users.first();
+
 if (!transferAmount || isNaN(transferAmount)) return message.channel.send(`Sorry ${message.author}, that's an invalid amount.`);
 if (transferAmount > currentAmount) return message.channel.send(`Sorry ${message.author}, you only have ${currentAmount}.`);
 if (transferAmount <= 0) return message.channel.send(`Please enter an amount greater than zero, ${message.author}.`);
