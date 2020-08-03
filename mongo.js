cca76003a92d8eca2aaa1cca153225278536b055
Cheetah <dariuswilsonxu@gmail.com>
2020-08-03 01:44:32 -0500

    d

--- mongo.js
+++ mongo.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-const { mongoPass } = require('./config.json')
-const mongoPath = `mongodb+srv://Darius:${mongoPass}@discordbot.3bz6j.mongodb.net/discordbot?retryWrites=true&w=majority`
+const { mongoPass } = require('./config.json');
+const mongoPath = `mongodb+srv://Darius:${mongoPass}@discordbot.3bz6j.mongodb.net/discordbot?retryWrites=true&w=majority`;
 
 module.exports = async () => {
-    await mongoose.connect(mongoPath, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    return mongoose
-}
+	await mongoose.connect(mongoPath, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	});
+	return mongoose;
+};
