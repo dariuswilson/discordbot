3f1c4efd74397fcd6252b2f649356a24087b8382
Cheetah <dariuswilsonxu@gmail.com>
2020-08-04 01:51:12 -0500

    d

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -9,10 +9,10 @@ module.exports = {
 		await mongo().then(async (mongoose) => {
 			try {
 				console.log('Connected to mongodb!');
-				const found = await profileSchema.findOne(userId: user);
+				const found = await profileSchema.findOne({ userId: user });
 				if (message.author) {
-					message.channel.send(`You have recieved your daily amount of {amount}`);
-				console.log(found)
+					message.channel.send('You have recieved your daily amount of {amount}');
+					console.log(found);
 				}
 			} finally {
 				mongoose.connection.close();
@@ -22,17 +22,17 @@ module.exports = {
 };
 
 
-		// const timeout = 86400000;
-		// const amount = 10;
-		// const daily = await mongo.findOne()(`daily_${message.guild.id}_${message.author.id}`);
+// const timeout = 86400000;
+// const amount = 10;
+// const daily = await mongo.findOne()(`daily_${message.guild.id}_${message.author.id}`);
 
-		// if(daily !== null && timeout - (Date.now () - daily) > 0) {
-		// 	const time = ms(timeout - (Date.now() - daily));
+// if(daily !== null && timeout - (Date.now () - daily) > 0) {
+// 	const time = ms(timeout - (Date.now() - daily));
 
-		// 	return message.channel.send(`You've already collected your daily award. Come back in ${time.days}d, ${time.hours}h, ${time.minutes}m, and ${time.seconds}s`);
-		// } else {
-		// 	mongo.add(`money_${message.guild.id}_${message.author.id}`, amount);
-		// 	mongo.set(`daily_${message.guild.id}_${message.author.id}`, Date.now());
+// 	return message.channel.send(`You've already collected your daily award. Come back in ${time.days}d, ${time.hours}h, ${time.minutes}m, and ${time.seconds}s`);
+// } else {
+// 	mongo.add(`money_${message.guild.id}_${message.author.id}`, amount);
+// 	mongo.set(`daily_${message.guild.id}_${message.author.id}`, Date.now());
 
-		// 	message.channel.send(`Successfully added ${amount} of coins to your account!`);
-		// }
+// 	message.channel.send(`Successfully added ${amount} of coins to your account!`);
+// }
