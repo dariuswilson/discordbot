72c4f616ff670abc55316b5194f523af1a864c38
Cheetah <dariuswilsonxu@gmail.com>
2020-08-07 00:12:05 -0500

    a

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -6,27 +6,14 @@ module.exports = {
 	commands: ['daily'],
 	cooldown: 60 * 1440,
 	callback: async (message, args, text) => {
-		const amount = 10;
 		await mongo().then(async (mongoose) => {
 			try {
 				const user = message.author.id;
 				console.log('Connected to mongodb!');
-				const storeddate = await profileSchema.findOne({userId : user}, {_id:0 });
-				const Tdate = new Date(); // Don't worry about this for now, I'll explain later.
-				const tdm = Tdate.getTime();
-				console.log(storeddate);
-				if (!storeddate) {
-					await economy.dailyCoins(user, 10, Tdate); // Don't worry about Tdate, I'll explain later.
+				if (user === '!daily') {
+					await economy.dailyCoins(user, 10); // Don't worry about Tdate, I'll explain later.
 					message.channel.send('You have received your daily amount of 10 coins! Please try again in 24 hours.');
 					return;
-				}
-				const sdm = storeddate.getTime() + 86400000;
-				if (tdm >= sdm) {
-					await economy.dailyCoins(user, 10, Tdate); // Don't worry about Tdate, I'll explain later.
-					message.channel.send('You have received your daily amount of 10 coins! Please try again in 24 hours.');
-					return;
-				} else {
-					return message.channel.send('Please wait for a full 24 hours before running this command again.');
 				}
 			} finally {
 				mongoose.connection.close();
