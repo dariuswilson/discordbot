3192c0c5f3ae88c04c34608ce3e3ba961a5066b1
Cheetah <dariuswilsonxu@gmail.com>
2020-08-07 13:06:26 -0500

    a

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -10,11 +10,9 @@ module.exports = {
 			try {
 				const user = message.author.id;
 				console.log('Connected to mongodb!');
-				if (user === '!daily') {
-					await economy.dailyCoins(user, 10); // Don't worry about Tdate, I'll explain later.
-					message.channel.send('You have received your daily amount of 10 coins! Please try again in 24 hours.');
-					return;
-				}
+				await economy.dailyCoins(user, 10); // Don't worry about Tdate, I'll explain later.
+				message.channel.send('You have received your daily amount of 10 coins! Please try again in 24 hours.');
+				return;
 			} finally {
 				mongoose.connection.close();
 			}
